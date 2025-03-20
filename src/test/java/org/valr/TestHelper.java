4cfe73b95302b21de137e5f66b4f2b8ad8a30036
Yashay <yashayrampershad@gmail.com>
2025-03-20 17:54:26 -0400

    [Prototype][2/2] Add test helper methods

--- src/test/java/org/valr/TestHelper.java
+++ src/test/java/org/valr/TestHelper.java
@@ -1,6 +1,8 @@
 package org.valr;
 
 import org.valr.model.Order;
+import org.valr.model.Pool;
+import org.valr.model.Trade;
 import org.valr.model.enums.ExchangePair;
 import org.valr.model.enums.Side;
 import org.valr.model.enums.TimeInForce;
@@ -14,6 +16,10 @@ public class TestHelper {
         return BigDecimal.valueOf(number);
     }
 
+    public static Order createOrder(Side side, BigDecimal price, BigDecimal quantity) {
+        return createOrder("userId", side, price, quantity);
+    }
+
     public static Order createOrder(String userId, Side side, BigDecimal price, BigDecimal quantity) {
         Order order = new Order();
         order.setUserId(userId);
@@ -24,7 +30,26 @@ public class TestHelper {
         order.setQuantity(quantity);
         order.setTimeInForce(TimeInForce.FOK);
         order.setTimestamp(Instant.now());
+        order.setSequence(Instant.now().toEpochMilli());
         return order;
+    }
+
+    public static Pool createPool(Order makerOrder) {
+        Pool pool = new Pool(makerOrder.getSide(), makerOrder.getExchangePair(), makerOrder.getPrice());
+        pool.getOrders().add(makerOrder);
+        return pool;
+    }
+
+    public static Pool createPool(Side side, BigDecimal price, BigDecimal volume) {
+        Pool pool = new Pool(side, ExchangePair.BTCZAR, price);
+        pool.getVolume().updateAndGet(v -> v.add(volume));
+        return pool;
+    }
+
+    public static Trade createTrade(Side side, BigDecimal price, BigDecimal quantity) {
+        Order takerOrder = createOrder(side, price, quantity);
+        Order makerOrder = createOrder(side == Side.BUY ? Side.SELL : Side.BUY, price, quantity);
+        return new Trade(takerOrder, makerOrder, price, quantity);
     }
 
 }
