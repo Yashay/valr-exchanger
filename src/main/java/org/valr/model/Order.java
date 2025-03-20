5ed768f88568ac99255f3759cbfe693054fbe4e7
Yashay <yashayrampershad@gmail.com>
2025-03-20 17:20:17 -0400

    [Prototype][1/2] Add validation annotations on Order Model

--- src/main/java/org/valr/model/Order.java
+++ src/main/java/org/valr/model/Order.java
@@ -1,5 +1,7 @@
 package org.valr.model;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.valr.model.enums.ExchangePair;
 import org.valr.model.enums.Side;
@@ -16,15 +18,27 @@ import java.util.UUID;
 @Setter
 public class Order implements Comparable<Order> {
     private String orderId = UUID.randomUUID().toString();
+
     private String userId;
     private Long sequence;
 
+    @NotNull(message = "TimeInForce is required")
     private TimeInForce timeInForce;
+
+    @NotNull(message = "Side is required")
     private Side side;
+
+    @NotNull(message = "Currency exchange pair is required")
     private ExchangePair exchangePair;
 
+    @NotNull
+    @DecimalMin(value = "0.000001", message = "Price must be greater than zero")
     private BigDecimal price;
+
     private BigDecimal initialQuantity;
+
+    @NotNull
+    @DecimalMin(value = "0.000001", message = "Quantity must be greater than zero")
     private BigDecimal quantity;
 
     private Instant timestamp = Instant.now();
