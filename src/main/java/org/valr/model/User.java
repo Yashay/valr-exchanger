25164882543bce78165c7c14e8ccdadd7ec18a45
Yashay <yashayrampershad@gmail.com>
2025-03-21 07:37:06 -0400

    [Prototype][1/2][FIX] Add User json annotated constructor

--- src/main/java/org/valr/model/User.java
+++ src/main/java/org/valr/model/User.java
@@ -1,6 +1,10 @@
 package org.valr.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
+
+import java.util.UUID;
 
 @Data
 @AllArgsConstructor
@@ -9,4 +13,14 @@ public class User {
     private final String userId;
     private final String username;
     private final String password;
+
+    //TODO consider moving this to DTO
+    @JsonCreator
+    public User(
+            @JsonProperty("username") String username,
+            @JsonProperty("password") String password) {
+        this.userId = UUID.randomUUID().toString();
+        this.username = username;
+        this.password = password;
+    }
 }
