e48772ec6edbedfe1793e9eaf45b643d6daa168b
Yashay <yashayrampershad@gmail.com>
2025-03-21 07:32:48 -0400

    [Prototype][1/2] Refactor orderbook verticle to enable validation

--- src/main/java/org/valr/verticles/OrderBookVerticle.java
+++ src/main/java/org/valr/verticles/OrderBookVerticle.java
@@ -1,7 +1,6 @@
 package org.valr.verticles;
 
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
@@ -19,27 +18,28 @@ public class OrderBookVerticle extends AbstractVerticle {
     }
 
     private void setupRoutes(Router router, AuthMiddleware authMiddleware) {
-        router.post("/api/orders/limit").handler(authMiddleware::authenticate).handler(this::placeLimitOrder);
-        router.get("/api/orderbook").handler(this::getOrderBookSnapshot);
+        router.post("/api/orders/limit")
+                .handler(authMiddleware::authenticate)
+                .handler(this::placeLimitOrder);
+        router.get("/api/orderbook")
+                .handler(this::getOrderBookSnapshot);
     }
 
     private void getOrderBookSnapshot(RoutingContext context) {
-        context.request().bodyHandler(body -> {
-            JsonObject orderBookSnapshot = orderBookService.getOrderBookSnapshot("BTCZAR");
-            context.response().setStatusCode(201).end(orderBookSnapshot.encodePrettily());
-        });
+        //TODO this should not be statically coded
+        JsonObject orderBookSnapshot = orderBookService.getOrderBookSnapshot("BTCZAR");
+        context.response().setStatusCode(201).end(orderBookSnapshot.encodePrettily());
     }
 
     private void placeLimitOrder(RoutingContext context) {
-        context.request().bodyHandler(body -> {
-            String userId = AuthMiddleware.getUserIdFromContext(context);
-            Order order = Json.decodeValue(body, Order.class);
-            JsonObject orderInformation = orderBookService.placeLimitOrder(userId, order);
-            if(orderInformation.isEmpty()) {
-                context.response().setStatusCode(201).end("Created");
-            } else {
-                context.response().setStatusCode(400).end(orderInformation.encodePrettily());
-            }
-        });
+        String userId = AuthMiddleware.getUserIdFromContext(context);
+        JsonObject data = context.body().asJsonObject();
+        Order order = data.mapTo(Order.class);
+        JsonObject orderInformation = orderBookService.placeLimitOrder(userId, order);
+        if (orderInformation.isEmpty()) {
+            context.response().setStatusCode(201).end("Created");
+        } else {
+            context.response().setStatusCode(400).end(orderInformation.encodePrettily());
+        }
     }
 }
