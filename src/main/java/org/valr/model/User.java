3f970c1b2096ccafc18c1c723981b6f5d32e7c0e
Yashay <yashayrampershad@gmail.com>
2025-03-21 08:03:26 -0400

    [Prototype][1/2] Add validation to user model object

--- src/main/java/org/valr/model/User.java
+++ src/main/java/org/valr/model/User.java
@@ -2,6 +2,8 @@ package org.valr.model;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.util.UUID;
@@ -11,7 +13,13 @@ import java.util.UUID;
 @ToString
 public class User {
     private final String userId;
+
+    @NotNull(message = "Username is required")
+    @NotBlank(message = "Username cannot be blank")
     private final String username;
+
+    @NotNull(message = "Password is required")
+    @NotBlank(message = "Password cannot be blank")
     private final String password;
 
     //TODO consider moving this to DTO
