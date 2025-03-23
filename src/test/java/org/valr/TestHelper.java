311ac67164701467fbf703cb582b305c1426623d
Yashay <yashayrampershad@gmail.com>
2025-03-23 07:47:59 -0400

    [Prototype][2/2] Update test helper to include multiple overloaded createOrder methods

--- src/test/java/org/valr/TestHelper.java
+++ src/test/java/org/valr/TestHelper.java
@@ -9,26 +9,18 @@ import org.valr.model.enums.TimeInForce;
 
 import java.math.BigDecimal;
 import java.time.Instant;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class TestHelper {
+
+    private static final AtomicLong counter = new AtomicLong(0);
 
     public static BigDecimal NUMBER(int number) {
         return BigDecimal.valueOf(number);
     }
 
-    public static Order createOrder(Side side, BigDecimal price, BigDecimal quantity) {
-        return createOrder("userId", side, price, quantity, TimeInForce.GTC);
-    }
-
-    public static Order createOrder(Side side, BigDecimal price, BigDecimal quantity, TimeInForce timeInForce) {
-        return createOrder("userId", side, price, quantity, timeInForce);
-    }
-
-    public static Order createOrder(String userId, Side side, BigDecimal price, BigDecimal quantity) {
-        return createOrder(userId, side, price, quantity, TimeInForce.GTC);
-    }
-
-    public static Order createOrder(String userId, Side side, BigDecimal price, BigDecimal quantity, TimeInForce timeInForce) {
+    // TODO this needs some refactoring from here on
+    public static Order createOrder(String userId, Side side, BigDecimal price, BigDecimal quantity, TimeInForce timeInForce, Instant timeCreated, long sequence) {
         Order order = new Order();
         order.setUserId(userId);
         order.setSide(side);
@@ -37,9 +29,33 @@ public class TestHelper {
         order.setInitialQuantity(quantity);
         order.setQuantity(quantity);
         order.setTimeInForce(timeInForce);
-        order.setTimestamp(Instant.now());
-        order.setSequence(Instant.now().toEpochMilli());
+        order.setTimestamp(timeCreated);
+        order.setSequence(sequence);
         return order;
+    }
+
+    public static Order createOrder(Side side, BigDecimal price, BigDecimal quantity) {
+        return createOrder("userId", side, price, quantity, TimeInForce.GTC, Instant.now(), counter.incrementAndGet());
+    }
+
+    public static Order createOrder(Side side, BigDecimal price, BigDecimal quantity, TimeInForce timeInForce) {
+        return createOrder("userId", side, price, quantity, timeInForce, Instant.now(), counter.incrementAndGet());
+    }
+
+    public static Order createOrder(String userId, Side side, BigDecimal price, BigDecimal quantity) {
+        return createOrder(userId, side, price, quantity, TimeInForce.GTC, Instant.now(), counter.incrementAndGet());
+    }
+
+    public static Order createOrder(String userId, Side side, BigDecimal price, BigDecimal quantity, TimeInForce timeInForce) {
+        return createOrder(userId, side, price, quantity, timeInForce, Instant.now(), counter.incrementAndGet());
+    }
+
+    public static Order createOrder(String userId, Side side, BigDecimal price, BigDecimal quantity, TimeInForce timeInForce, long sequence) {
+        return createOrder(userId, side, price, quantity, timeInForce, Instant.now(), sequence);
+    }
+
+    public static Order createOrder(String userId, Side side, BigDecimal price, BigDecimal quantity, TimeInForce timeInForce, Instant timeCreated) {
+        return createOrder(userId, side, price, quantity, timeInForce, timeCreated, counter.incrementAndGet());
     }
 
     public static Pool createPool(Order makerOrder) {
@@ -59,5 +75,4 @@ public class TestHelper {
         Order makerOrder = createOrder(side == Side.BUY ? Side.SELL : Side.BUY, price, quantity);
         return new Trade(takerOrder, makerOrder, price, quantity);
     }
-
 }
