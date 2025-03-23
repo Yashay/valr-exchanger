787df8cc34c8b71c2e25a13bdaf9818caa08c28f
Yashay <yashayrampershad@gmail.com>
2025-03-23 08:47:26 -0400

    [Prototype][2/2][Style] Removed unused method

--- src/main/java/org/valr/registry/AppRegistry.java
+++ src/main/java/org/valr/registry/AppRegistry.java
@@ -2,14 +2,9 @@ package org.valr.registry;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-import io.vertx.core.Vertx;
 
 public class AppRegistry {
     // not final for testing purposes
     public static Injector injector = Guice.createInjector(new AppModule());
-
-    public static Vertx getVertxInstance() {
-        return injector.getInstance(Vertx.class);
-    }
 
 }
