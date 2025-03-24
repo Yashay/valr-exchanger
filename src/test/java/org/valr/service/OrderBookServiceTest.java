bad0f73cb170ce1a2fbf4723b171ac876a168d43
Yashay <yashayrampershad@gmail.com>
2025-03-24 02:51:22 -0400

    [Prototype][2/2] Update Orderbook Test to include get orderbook with orders test

--- src/test/java/org/valr/service/OrderBookServiceTest.java
+++ src/test/java/org/valr/service/OrderBookServiceTest.java
@@ -1,5 +1,6 @@
 package org.valr.service;
 
+import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
 import org.apache.commons.lang3.tuple.Pair;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,11 +17,11 @@ import org.valr.repository.OrderBookRepository;
 import java.math.BigDecimal;
 import java.util.concurrent.ConcurrentSkipListMap;
 import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.atomic.AtomicLong;
 
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.valr.TestHelper.NUMBER;
-import static org.valr.TestHelper.createOrder;
+import static org.valr.TestHelper.*;
 
 class OrderBookServiceTest {
 
@@ -111,16 +112,42 @@ class OrderBookServiceTest {
     }
 
     @Test
-    void testGetOrderBookSnapshot() {
+    void testGetOrderBookSnapshotEmpty() {
         when(orderBookRepository.getSellPoolMap()).thenReturn(new ConcurrentSkipListMap<BigDecimal, Pool>());
         when(orderBookRepository.getBuyPoolMap()).thenReturn(new ConcurrentSkipListMap<BigDecimal, Pool>());
-        when(orderBookRepository.getOrderCounter()).thenReturn(new java.util.concurrent.atomic.AtomicLong(42));
+        when(orderBookRepository.getOrderCounter()).thenReturn(new AtomicLong(42));
 
         JsonObject snapshot = orderBookService.getOrderBookSnapshot("BTCZAR");
 
         assertNotNull(snapshot);
         assertEquals(0, snapshot.getJsonArray("Asks").size());
         assertEquals(0, snapshot.getJsonArray("Bids").size());
+        assertEquals(42L, snapshot.getLong("sequence"));
+        assertNotNull(snapshot.getInstant("createdAt"));
+    }
+
+    @Test
+    void testGetOrderBookSnapshotWithOrders() {
+
+        ConcurrentSkipListMap<BigDecimal, Pool> sellPoolMap = new ConcurrentSkipListMap<>();
+        sellPoolMap.put(NUMBER(500), createPool(Side.BUY, NUMBER(50000), NUMBER(10)));
+        ConcurrentSkipListMap<BigDecimal, Pool> buyPoolMap = new ConcurrentSkipListMap<>();
+        buyPoolMap.put(NUMBER(500), createPool(Side.BUY, NUMBER(40000), NUMBER(10)));
+
+        when(orderBookRepository.getSellPoolMap()).thenReturn(sellPoolMap);
+        when(orderBookRepository.getBuyPoolMap()).thenReturn(buyPoolMap);
+        when(orderBookRepository.getOrderCounter()).thenReturn(new AtomicLong(42));
+
+        JsonObject snapshot = orderBookService.getOrderBookSnapshot("BTCZAR");
+
+        String asksPrice = snapshot.getJsonArray("Asks").getJsonObject(0).getString("price");
+        String bidsPrice = snapshot.getJsonArray("Bids").getJsonObject(0).getString("price");
+
+        assertNotNull(snapshot);
+        assertEquals(1, snapshot.getJsonArray("Asks").size());
+        assertEquals(1, snapshot.getJsonArray("Bids").size());
+        assertEquals(NUMBER(50000), NUMBER(asksPrice));
+        assertEquals(NUMBER(40000), NUMBER(bidsPrice));
         assertEquals(42L, snapshot.getLong("sequence"));
         assertNotNull(snapshot.getInstant("createdAt"));
     }
