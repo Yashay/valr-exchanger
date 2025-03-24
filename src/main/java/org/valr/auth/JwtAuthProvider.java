22f4040d1fd3e43f221d02cff479912920cb4a6f
Yashay <yashayrampershad@gmail.com>
2025-03-24 03:08:10 -0400

    [Prototype][2/2] Add testing instruction comment n JwtAuthProvider

--- src/main/java/org/valr/auth/JwtAuthProvider.java
+++ src/main/java/org/valr/auth/JwtAuthProvider.java
@@ -15,7 +15,7 @@ public class JwtAuthProvider {
     private static final String JWT_HASHING_ALGORITHM = "HS256";
     private static final String KEY_HASHING_ALGORITHM = "HmacSHA256";
     private static final int EXPIRATION_TIME = 60 * 60 * 24 * 30;
-    // hard to test
+    // for testing set the value arbitrarily otherwise restarts require new JWT
     private static final String SECRET_KEY = "Hello-World!";
     private final JWTAuth jwtAuth;
 
